5fd581440f00a1d1df414aa40bfe56fba6019f6b
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-15 09:21:41 +0300

    introductory message program

---
+++ basic.py
@@ -0,0 +1,7 @@
+#name function
+name = input("What is your name?")
+#age function
+age = input("How old are you?")
+#colour function
+color = input("Whatis your favourite color?")
+print(f"Hello {name}, you are {age} years old and your favourite colour is {color}.")
