bd0fc05bc57a42182722ddaa60fc47258ceb1a4e
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-15 20:11:51 +0300

    This program prints your numbers in reverse

--- reverse.py
+++ reverse.py
@@ -1,8 +1,11 @@
 #This program rewrites your numbers in reverse
+
 #input number function
 first_number = int(input("Enter your first number "))
 second_number = int(input("Enter your second number "))
 third_number = int(input("Enter your third number "))
+
 #reverse fn
 reverse = third_number, second_number, first_number
+
 print(f"Your numbers in reverse are:", {reverse})
