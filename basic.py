274fb262a7c6659acb9bec2b11ae005d86f97c7d
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-15 20:17:28 +0300

    basic civil introduction

--- basic.py
+++ basic.py
@@ -1,24 +1,29 @@
-# #basic introduction
-# #name function
-# name = input("What is your name?")
-# #age function
-# age = input("How old are you?")
-# #colour function
-# color = input("Whatis your favourite color?")
-# print(f"Hello {name}, you are {age} years old and your favourite colour is {color}.")
+#basic introduction
+
+#name function
+name = input("What is your name? ")
+#age function
+age = input("How old are you? ")
+#colour function
+color = input("What is your favourite color? ")
+print(f"Hello {name}, you are {age} years old and your favourite colour is {color}.")
 
 
-# #greeting
-# #name function
-# first_name = input("What is your first name? 
-# second_name = input("What is your second name? ")
-# #message
-# print(f"Hi {first_name} {second_name}, nice to meet you!")
+#greeting
+
+#name function
+first_name = input("What is your first name? ")
+second_name = input("What is your second name? ")
+#message
+print(f"Hi {first_name} {second_name}, nice to meet you!")
 
 
 
 #favourite food
+
 #input food
 favourite_food = input("What is your favourite food? ")
 print(f"Yum! {favourite_food} sounds delicious.")
+
+
 
