dad49326ad06b50aee25a52fd459d2ab003a280d
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-15 20:13:48 +0300

    This program calculates your age in dog years

--- age.py
+++ age.py
@@ -1,6 +1,9 @@
 #age in dog years
+
 #human age input function
 human_years = int(input("How old are you? "))
+
 #age in dog years
 dog_age = human_years * 7
+
 print(f" Your age in dog years is:", dog_age)
