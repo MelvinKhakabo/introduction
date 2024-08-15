a7113066fdf6e57ebef2220aeaf026a624b80d5b
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-15 20:08:03 +0300

    This program converts temperature in degree celsius to fahrenheit

--- temp.py
+++ temp.py
@@ -1,5 +1,10 @@
+#This program converts temperature in degree celsius to fahreinheit
+
 #input temperature in degree celsius
 celsius = int(input("What is your current temperature in degree celsius? "))
+
 #input temperature in farenheit
 fahrenheit = (celsius * 9/5) + 32
+
 print(f"Your temperature in fahrenheit is:", fahrenheit)
+
