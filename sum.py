0aac2188caa3c9dfba473dea91aaa068f1b4d180
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-15 09:44:42 +0300

    basic mathematical operations

---
+++ sum.py
@@ -0,0 +1,7 @@
+#input num1
+first_number = int(input("Provide the first number? "))
+#input num2
+second_number = int(input("Provide the second number? "))
+#add
+sum = first_number + second_number
+print("The sum is:", sum)
