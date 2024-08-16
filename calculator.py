4fe729268b99119762973d167706b33838f75f5e
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-16 09:45:28 +0300

    This program runs basic math calculations

---
+++ calculator.py
@@ -0,0 +1,26 @@
+#This program runs basic math calculations
+
+#input number functions
+#input first number
+first_number = int(input("Provide your first number? "))
+#input second number
+second_number = int(input("Provide your second number? "))
+
+# #addition
+# sum = first_number + second_number
+# print(f"The sum is:", sum)
+
+
+# #subtraction
+# difference = first_number - second_number
+# print(f"The difference is:", difference)
+
+
+# #multiplication
+# product = first_number * second_number
+# print(f"The product is:", product)
+
+
+#division
+result = first_number / second_number
+print(f"The result is:", result)
