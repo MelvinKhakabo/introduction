183d09cc43ae7b65ae37e4d78a905b09e41970cf
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-08-16 09:33:11 +0300

    This program calculates your discounted price

--- discount.py
+++ discount.py
@@ -1,5 +1,11 @@
+#program that calculates your discounted price
 #input original price
 price = int(input("Please input the original price of the item "))
+
 # input discount percentage
-disc = int(input(""))
+discount = int(input("Input a percentage discount "))
+
 #calculate discounted price
+discounted_price = price * (100 - discount) / 100
+
+print(f"The discounted price is:", discounted_price)
